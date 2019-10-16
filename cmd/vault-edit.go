1a973a4d25a598eb5307861a087022948e32229a
Roger Liang <pinguroger@gmail.com>
2019-10-16 23:19:51 +0000

    Added vault edit feature

---
+++ cmd/vault-edit.go
@@ -0,0 +1,126 @@
+package cmd
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"github.com/liangrog/cfctl/pkg/utils/editor"
+	"github.com/liangrog/cfctl/pkg/utils/i18n"
+	"github.com/liangrog/cfctl/pkg/utils/templates"
+	"github.com/liangrog/vault"
+	"github.com/spf13/cobra"
+)
+
+const (
+	// cfctl editor env
+	EDITOR_ENV = "CFCTL_EDITOR"
+)
+
+var (
+	vaultEditShort = i18n.T("Edit vault file in a seperate terminal")
+
+	vaultEditLong = templates.LongDesc(i18n.T(`
+		Edit an encrypted vault file by openning a seperate
+		terminal using default editor.`))
+
+	vaultEditExample = templates.Examples(i18n.T(`
+		# Edit a vault file named 'secrect.yaml'
+		cfctl vault edit secret.yaml
+
+		# Edit a vault file 'secret.yaml' with a password argument
+		cfctl vault edit secret.yaml --vault-password Password1
+
+		# Edit a vault file 'secret.yaml' providing a vault password file in $HOME/vault-password-file
+		cfctl vault edit secret.yaml --vault-password-file $HOME/vault-password-file`))
+)
+
+// Register sub commands
+func init() {
+	cmd := getCmdVaultEdit()
+
+	CmdVault.AddCommand(cmd)
+}
+
+// cmd: encrypt
+func getCmdVaultEdit() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "edit",
+		Short:   vaultEditShort,
+		Long:    vaultEditLong,
+		Example: fmt.Sprintf(vaultEditExample),
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 || len(args) > 1 {
+				return errors.New("Need to provide one filename only in command argument")
+			}
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			err := editVaultFile(
+				cmd.Flags().Lookup(CMD_VAULT_PASSWORD).Value.String(),
+				cmd.Flags().Lookup(CMD_VAULT_PASSWORD_FILE).Value.String(),
+				args,
+			)
+
+			silenceUsageOnError(cmd, err)
+
+			return err
+
+		},
+	}
+
+	return cmd
+}
+
+// Edit encrypted vault file in a seperate terminal using default editor
+func editVaultFile(password, passwordFile string, files []string) error {
+	passwords, err := GetPasswords(password, passwordFile, false, false)
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadFile(files[0])
+	if err != nil {
+		return err
+	}
+
+	// Try every given password
+	var originalPlainText []byte
+	var foundPass string
+
+	decrypted := false
+	for _, p := range passwords {
+		originalPlainText, err = vault.Decrypt(p, data)
+		if err == nil {
+			decrypted = true
+			foundPass = p
+			break
+		}
+	}
+
+	if !decrypted {
+		return errors.New(fmt.Sprintf("Failed to decrypt %s using all given password", files[0]))
+	}
+
+	buf := bytes.NewBuffer(originalPlainText)
+
+	edit := editor.NewDefaultEditor([]string{EDITOR_ENV})
+	editedPlainText, _, err := edit.LaunchTempFile(fmt.Sprintf("%s-edit-", filepath.Base(files[0])), filepath.Ext(files[0]), buf)
+	if err != nil {
+		return err
+	}
+
+	encryptedText, err := vault.Encrypt(editedPlainText, foundPass)
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(files[0], encryptedText, 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,18 +1,20 @@
 module github.com/liangrog/cfctl
 
 require (
-	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/MakeNowJust/heredoc v1.0.0
 	github.com/aws/aws-sdk-go v1.16.18
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/google/uuid v1.1.1
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/liangrog/ds v0.0.0-20190409010058-ef878b422520
 	github.com/liangrog/vault v0.0.0-20191003051523-b14798d78aaf
-	github.com/spf13/cobra v0.0.3
-	github.com/spf13/viper v1.3.1
+	github.com/russross/blackfriday v1.5.2
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc
-	gopkg.in/yaml.v2 v2.2.2
+	gopkg.in/yaml.v2 v2.2.4
+	k8s.io/klog v1.0.0
+	k8s.io/kubectl v0.0.0-20191015071726-6d12ae1ac20b
 )
 
 go 1.13
